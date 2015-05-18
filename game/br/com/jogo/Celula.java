128842a454ff9f6d8d03fc8e6a040c8c6abca390
hguerra <heitorguerrac@gmail.com>
2015-05-18 17:19:25 -0300

    enviando

--- game/br/com/jogo/Celula.java
+++ game/br/com/jogo/Celula.java
@@ -42,8 +42,8 @@ public class Celula extends JButton implements Cloneable {
 				*/
 				
 				
-				Conexao.preto.setValido(false);
-				Conexao.amarelo.setValido(false);
+				//Conexao.preto.setValido(false);
+				//Conexao.amarelo.setValido(false);
 
 				String sendout = "_JOG_" + "_X_" + getColuna() + "_Y_"
 						+ getLinha() + "_JOGCOR_" + dono.getNome() + "!";
